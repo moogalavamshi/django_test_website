114092b7a365a017083cf570289bf7cda6621664
Vamshi <mr.vamshi123@gmail.com>
2017-10-29 17:24:25 -0300

    added templates for views

--- music/views.py
+++ music/views.py
@@ -1,23 +1,20 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
+
 
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail_view(request, album_id):
     # html = '<h2> the album id is ' + album_id + '</h2>'
     html = ''
-    album = Album.objects.filter(id=album_id)
-    for item in album:
-        html = '<h1>' + str(item.id) + '</h1>' + '<h2>' + item.album_title + '</h2>' + '<img height = "200" src = "' + item.album_logo + '"</img>'
 
     return HttpResponse(html)
